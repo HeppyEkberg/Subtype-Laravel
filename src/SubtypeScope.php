5fa5f180765d7288a869853332f7befc0510c252
Mikael Nilsson <mikael.nilsson@elicit.se>
2020-04-24 09:12:15 +0200

    Updated extended class

--- src/SubtypeScope.php
+++ src/SubtypeScope.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\ScopeInterface;
+use Illuminate\Database\Eloquent\Scope;
 
-class SubtypeScope implements ScopeInterface
+class SubtypeScope implements Scope
 {
     /**
      * Apply the scope to a given Eloquent query builder.
